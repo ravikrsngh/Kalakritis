b8d0d0af550ff129e2b394b0b89f72a069cfb38e
Ravi Kumar Singh <ravikrsngh.rks@gmail.com>
2023-09-29 16:46:55 +0530

    Added mimetypes

--- kalakritis_backend/settings.py
+++ kalakritis_backend/settings.py
@@ -14,12 +14,16 @@ from pathlib import Path
 import os
 import environ
 from datetime import timedelta
+import mimetypes
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
 environ.Env.read_env()
+
+mimetypes.add_type("application/javascript", ".js", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
