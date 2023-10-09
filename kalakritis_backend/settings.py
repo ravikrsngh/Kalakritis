42e45268fc2541c257470780b2b3ea49aab744ec
Ravi Kumar Singh <ravikrsngh.rks@gmail.com>
2023-10-09 18:29:55 +0530

    Static File fix 1

--- kalakritis_backend/settings.py
+++ kalakritis_backend/settings.py
@@ -32,7 +32,7 @@ mimetypes.add_type("application/javascript", ".js", True)
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
